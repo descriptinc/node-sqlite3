128c734583a84081c9580aadcff90ba14dd1c80e
Eric Suh <esuh@descript.com>
2021-10-27 17:12:31 -0700

    [v5.0.3-rc.5] Bump SQLite to v3.35.0 and package in Github

--- scripts/build-local.bat
+++ scripts/build-local.bat
@@ -13,44 +13,23 @@ IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 IF EXIST node_modules ECHO deleting node_modules && RD /Q /S node_modules
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
-GOTO VS2015
-
-ECHO                 ============================
-ECHO                           VS2013
-ECHO                 ============================
-SET nodejs_version=4.4.2
-SET platform=x86
-SET msvs_toolset=12
-SET TOOLSET_ARGS=
-
-CALL scripts\build-appveyor.bat
-IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-GOTO DONE
-
-:VS2015
-
 IF EXIST lib\binding ECHO deleting lib/binding && RD /Q /S lib\binding
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 IF EXIST node_modules ECHO deleting node_modules && RD /Q /S node_modules
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
 
 ECHO                 ============================
-ECHO                           VS2015
+ECHO                           VS2019
 ECHO                 ============================
-SET nodejs_version=0.10.40
+SET nodejs_version=14.18.1
 ECHO SET nodejs_version^: %nodejs_version%
 SET platform=x64
 ECHO SET platform^: %platform%
-SET msvs_toolset=14
+SET msvs_version=2019
 ECHO SET msvs_toolset^: %msvs_toolset%
-::SET TOOLSET_ARGS=--dist-url=https://s3.amazonaws.com/mapbox/node-cpp11 --toolset=v140
 
 CALL scripts\build-appveyor.bat
 IF %ERRORLEVEL% NEQ 0 GOTO ERROR
-
-
-
 
 GOTO DONE
 
